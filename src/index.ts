28e704b73f09017e9a93a42a1df6719af2fb6ea0
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-04 14:37:40 +0200

    Created switch for every supported methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-import { handleRequest } from './handler'
+import { handleRequest, handleOptions } from './handler'
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+addEventListener('fetch', (event) => {
+  const { request } = event
+
+  switch (request.method) {
+    // Handle requests
+    case 'GET':
+    case 'HEAD':
+    case 'POST':
+      return event.respondWith(handleRequest(request))
+
+    case 'OPTIONS':
+      return event.respondWith(handleOptions(request))
+
+    // If no good option then return error
+    default:
+      return event.respondWith(
+        new Response(null, {
+          status: 405,
+
+          statusText: 'Method Not Allowed',
+        }),
+      )
+  }
 })
